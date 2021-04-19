8a0cb731c2c6bc9a587e72d44ad9c492c0b1f883
木村毅樹 <80314997+takeki-kimura@users.noreply.github.com>
2021-04-19 16:14:07 +0900

    Revert "購入者情報保存機能実装"
    This reverts commit 69f86f22ad2197d7ee591c0883d7067eee231a44.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-
   get 'items/search'
 
-  resources :items do
-    resources :orders, only: [:index, :create]
-  end
+  resources :items
 end
